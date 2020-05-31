2cd2a9272ce89a2fdcd27706ffd1b0f9eee60ad0
Erik Ahumada <costerik@hotmail.com>
2020-05-30 19:46:07 -0500

    added eslint, prettier, storybook and routing setup

---
+++ src/components/movie-card/__stories__/movie-card.story.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MovieCard from '../index';
+import {ReactElement} from 'react';
+
+export default {
+  title: 'MovieCard',
+  component: MovieCard,
+};
+
+export const Basic = (): ReactElement => <MovieCard />;
---
+++ src/components/movie-card/movie-card.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {ReactElement} from 'react';
+import style from './movie-card.module.scss';
+
+function MovieCard(): ReactElement {
+  return <div className={style.bold}>Hello</div>;
+}
+
+export default MovieCard;
